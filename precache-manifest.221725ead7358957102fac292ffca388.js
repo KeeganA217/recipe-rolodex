24edd3ded0d6a183fbb524fecf2e3cb79d6610e9
Keegan <keegan.adams15@gmail.com>
2020-11-07 08:48:37 -0600

    Updates

---
+++ precache-manifest.221725ead7358957102fac292ffca388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7065a10ded005243905e9ae88f560d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "425b842fd46054b352a6",
+    "url": "/static/css/main.94eb82e7.chunk.css"
+  },
+  {
+    "revision": "3f08d0feb930e3d9ce81",
+    "url": "/static/js/2.5ff21db7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.5ff21db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425b842fd46054b352a6",
+    "url": "/static/js/main.f8c18bfe.chunk.js"
+  },
+  {
+    "revision": "55f43375bf0b3dcf001f",
+    "url": "/static/js/runtime-main.2f266397.js"
+  }
+]);
